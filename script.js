adbea864940675c8260eebe158221954046e79d2
yashjariwala01 <122197521+yashjariwala01@users.noreply.github.com>
2023-03-05 16:01:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,37 @@
 //your code here
+window.onload= function(){
+    var image=document.querySelectorAll('.image');
+    var boxes=document.querySelectorAll('.boxes');
+
+    image.addEventListener('dragstart',(e)=>{
+        console.log("dragstart");
+        
+        e.target.className+='.selected';
+        setTimeout(()=>{
+            e.target.className+='hide';
+        },0);
+       
+    });
+
+    image.addEventListener('dragend',()=>{
+        console.log("dragend");
+    });
+
+    boxes.forEach(box=>{
+        box.addEventListener('dragover',(e)=>{
+           e.preventDefault();
+        });
+        box.addEventListener('dragenter',()=>{
+
+        });
+        box.addEventListener('dragleave',()=>{
+
+        });
+        box.addEventListener('drop',()=>{
+
+        });
+    
+
+        });
+    }
+
